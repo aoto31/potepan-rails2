b182a8d21ad996140bdccf4f472f9b6ceb9e28a1
aoto <ton2poko@gmail.com>
2023-10-02 00:13:25 +0900

    ファイル作り直した

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :user_name, presence: true, length: { minimum: 3 }
+
+end
+end
